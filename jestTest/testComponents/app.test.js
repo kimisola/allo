c35af7465a306b183944b8473a779aeb5b8230fa
Ks <annieh7330@gmail.com>
2020-03-09 00:32:41 +0800

    complete feature of drag-drop

--- jestTest/testComponents/app.test.js
+++ jestTest/testComponents/app.test.js
@@ -1,84 +1,34 @@
-// import React from 'react';
-// import homePageReducer from '../../reducers/homePageReducer';
-// import renderer from 'react-test-renderer';
+import React from "react";
+import BoardLink from "../../components/BoardLink";
+import renderer from 'react-test-renderer';
 
-// //輸入 initial state 與 action 後，返回預期的 state
+const store = {
+  firebaseUid: "123456789",
+  userEmail: "jestTest@test.com",
+  userDisplayName: "Jest Test",
+  userPhotoURL: "www.com",
+  boardBackground: "www.com",
+  targetLink: "www.google.com",
+  boardName: "jestTest",
+}
 
-// describe('homePageReducer', () => {
-//   let HomePageReducer;
-//   beforeEach(() => {
-//     jest.resetModules();
-//     HomePageReducer = homePageReducer; // eslint-disable-line global-require
-//   });
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(
+    <BoardLink store={ store }/>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 
-//   describe("type: ADD_BEINVITED_DATA", () => {
-//     it("should show the initial load status", () => {
-//       const state = {
-//         beInvitedData: [],
-//         invitationData: [],
-//         gotBeInvitedData: false,
-//       };
-//       const result = HomePageReducer(state, {
-//         type: "ADD_BEINVITED_DATA",
-//         data: [],
-//       });
-//       expect(result).toEqual({
-//         ...state,
-//         gotBeInvitedData: true
-//       });
-//     });
-//   });
+  // // manually trigger the callback
+  tree.props.onClick();
+  // // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 
-//   describe("type: UPDATE_BEINVITED_DATA", () => {
-//     it("should show the initial load status", () => {
-//       const state = {
-//         beInvitedData: [
-//           { confirm: true },
-//           { confirm: null }
-//         ],
-//         invitationData: [],
-//         gotBeInvitedData: false,
-//       };
-//       const result = HomePageReducer(state, {
-//         type: "UPDATE_BEINVITED_DATA",
-//         beInvitedData: [
-//           { confirm: true },
-//           { confirm: null }
-//         ],
-//         index: 1,
-//         confirm: true
-//       });
-//       expect(result).toEqual({
-//         ...state,
-//         beInvitedData: [
-//           { confirm: true },
-//           { confirm: true }
-//         ],
-//       });
-//     });
-//   });
-
-
-
-// })
-
-
-// // test('Link changes the class when hovered', () => {
-// //   const component = renderer.create(
-// //     <CommentTags />,
-// //   );
-// //   let tree = component.toJSON();
-// //   expect(tree).toMatchSnapshot();
-
-// //   // // manually trigger the callback
-// //   // tree.props.onMouseEnter();
-// //   // // re-rendering
-// //   // tree = component.toJSON();
-// //   // expect(tree).toMatchSnapshot();
-
-// //   // // manually trigger the callback
-// //   // tree.props.onMouseLeave();
-// //   // // re-rendering
-// //   // tree = component.toJSON();
-// //   // expect(tree).toMatchSnapshot();
-// // });
+  // // // manually trigger the callback
+  // tree.props.onMouseLeave();
+  // // // re-rendering
+  // tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+  
+});
